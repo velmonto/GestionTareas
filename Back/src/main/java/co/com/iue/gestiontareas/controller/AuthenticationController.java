75c700b9268e203d03032a08e6727862a281cfe0
Daniel <davemon18@gmail.com>
2024-08-16 14:18:46 -0500

    Se agregan etiquetas para conexion con el front

--- Back/src/main/java/co/com/iue/gestiontareas/controller/AuthenticationController.java
+++ Back/src/main/java/co/com/iue/gestiontareas/controller/AuthenticationController.java
@@ -6,13 +6,11 @@ import co.com.iue.gestiontareas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/authentication")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
     @Autowired
--- Back/src/main/java/co/com/iue/gestiontareas/controller/SubtaskController.java
+++ Back/src/main/java/co/com/iue/gestiontareas/controller/SubtaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/subtask")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SubtaskController {
 
     @Autowired
